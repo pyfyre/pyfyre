238e33106e9f34d3b4fb13a240397d179e85d22f
Jabez Borja <jabez.natsu@gmail.com>
2021-03-21 16:20:55 +0800

    Update manager.py boilerplate for help function

--- bin/pynani.py
+++ bin/pynani.py
@@ -163,18 +163,19 @@ import sys
 
 print(sys.argv[1])
 
-if sys.argv[1] == "create-app":
-	try:
-		pynani.create_app(sys.argv[2])
-	except:
-		pynani.create_app("My Nani App")
-elif sys.argv[1] == "runserver":
-	try:
-		pynani.run_server(sys.argv[2])
-	except:
-		pynani.runserver()
-elif sys.argv[1] == "":
-	pynani.help()
+try:
+	if sys.argv[1] == "create-app":
+		try:
+			pynani.create_app(sys.argv[2])
+		except:
+			pynani.create_app("My Nani App")
+	elif sys.argv[1] == "runserver":
+		try:
+			pynani.run_server(sys.argv[2])
+		except:
+			pynani.runserver()
+except:
+	pynani.pynani_help()
     """)
 
     # close the files
