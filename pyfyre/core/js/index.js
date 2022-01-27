1cd7281ed9d9a4ed341b3af98d4de7284a7b2911
Jabez Borja <jabez.natsu@gmail.com>
2022-01-28 01:35:55 +0800

    remove: unused js file

--- pyfyre/core/js/index.js
+++
@@ -1,8 +0,0 @@
-// t
-window.addEventListener("load", () => {
-    var __BODY__ = document.getElementById("app-mount");
-    var t = document.createElement("h3");
-    t.innerHTML = "Transpiling please wait...";
-    t.style = "color: green;"
-    __BODY__.appendChild(t)
-})
