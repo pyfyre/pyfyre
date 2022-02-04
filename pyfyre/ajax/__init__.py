4661a2712bb32ce8d69c8a7edc951a6013d9c576
Jabez Borja <jabez.natsu@gmail.com>
2022-02-04 23:53:21 +0800

    fix: ajax requests error

--- pyfyre/ajax/__init__.py
+++ pyfyre/ajax/__init__.py
@@ -1,9 +1,9 @@
 from browser import ajax
 
 class Ajax:
-    def get(url, then):
-        req = ajax.ajax()
-        req.open('GET', url, True)
-        req.bind('complete', then)
-        req.set_header('content-type','application/x-www-form-urlencoded')
-        req.send()
+    """
+    Access Brython builtin Ajax.
+    """
+
+    def get(url, mode="json", then=None):
+        req = ajax.get(url, mode=mode, oncomplete=then)
