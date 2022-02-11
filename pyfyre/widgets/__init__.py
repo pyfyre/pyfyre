be44c736026a043700963ca9c7c62ac40eb79cfd
Jabez Borja <jabez.natsu@gmail.com>
2022-02-11 16:50:19 +0800

    tweak (glob): folder structure

--- pyfyre/widgets/__init__.py
+++ pyfyre/widgets/__init__.py
@@ -3,12 +3,12 @@ from pyfyre.widgets.container import Container
 from pyfyre.widgets.image import Image
 from pyfyre.widgets.link import Link
 from pyfyre.widgets.listbuilder import ListBuilder
-from pyfyre.widgets.states import UsesState
+from pyfyre.core.states import UsesState
 from pyfyre.widgets.text import Text
 from pyfyre.widgets.textinput import TextInput, TextInputController
 
 __all__ = [
     'Button', 'Container', 'Image',
     'Link', 'ListBuilder', 'UsesState', 'Clickable',
-    'Text', 'TextInput', 'TextInputController'
+    'Text', 'TextInput', 'TextInputController',
 ]
