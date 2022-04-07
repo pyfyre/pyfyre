85e20fb886826e9690c82dd5982c3dfd9d8030c3
jabezborja <jabez.natsu@gmail.com>
2022-04-06 22:34:11 -0500

    [contrib] Improve: Contrib development kit server

--- server.py
+++ server.py
@@ -8,41 +8,51 @@ recreating PyFyre apps again and again.
 
 from shutil import rmtree
 from livereload import Server
-from pyfyre.management import create_app, run_app
+from pyfyre.management import ManagementUtility
 import os, subprocess, threading
 
 def main():
     print("Starting PyFyre contributor development environment...")
 
+    os.system("title Contrib Server")
+
     server = Server()
+
+    management = ManagementUtility()
 
     if os.path.exists("__dev__"): rmtree("__dev__")
     
-    create_app("__dev__", "Contributor development environment")
+    management.create_app("__dev__", "Contributor development environment")
 
-    _directory = os.path.join("__dev__")
-    _build = os.path.join(_directory, "__serve__")
+    _directory = os.path.join(os.getcwd())
 
     def reload():
-        print("Detected PyFyre file changes, producing js...")
+        print("Reloading, producing js...")
 
-        os.chdir(os.path.join("pyfyre"))
+        os.chdir(os.path.join(os.getcwd(), "..", "pyfyre"))
         os.system("brython-cli --make_package pyfyre")
 
-        with open(os.path.join("pyfyre", "pyfyre.brython.js")) as file:
+        with open(os.path.join("pyfyre.brython.js")) as file:
             content = file.read()
-        with open(os.path.join(_build, "pyf_modules", "pyfyre.brython.js"), "w") as file:
+        with open(os.path.join(_directory, "pyf_modules", "modules.js"), "w") as file:
             file.write(content)
 
-    print(f"PyFyre server for contributing development is now running on port 5000")
+        os.remove("pyfyre.brython.js")
+
+        print("Success!")
+
+    print(f"PyFyre server for contributing development kit")
 
     def run():
-        subprocess.call('start /wait pyfyre runapp', shell=True)
+        subprocess.call('start /wait pyfyre runapp && title PyFyre Server', shell=True)
 
     app_thread = threading.Thread(target=run)
     app_thread.start()
 
-    server.watch(f"{os.getcwd()}/pyfyre/", reload)
-    server.serve(port=5000, host="localhost", root=os.path.join("__serve__"))
+    while True:
+        command = input("PyFyre CDK >> ")
+
+        if command.lower() == "s":
+            reload()
 
 if __name__ == '__main__': main()
