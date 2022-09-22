6ecf6c1962616f13cf857e4d93c1e80830ad47bb
NadieFiind <nadiefiind@gmail.com>
2022-09-22 15:51:28 +0800

    [Refactor] Update default template

--- cli/project_templates/default/src/index.py
+++ cli/project_templates/default/src/index.py
@@ -1,13 +1,27 @@
+from typing import List
 from pyfyre import render
-from pyfyre.widgets import Widget, TextWidget
+from pyfyre.widgets import *
+from browser import DOMEvent
 
 
-class App(Widget):
+class App(StatefulContainer):
 	def __init__(self) -> None:
-		super().__init__(
-			"h1",
-			children=[TextWidget("Welcome to PyFyre ✨")]
-		)
+		self.counter = 0
+		super().__init__(children=[
+			Paragraph(self.counter),
+			Button(self.increment, children=[TextWidget("Increase")])
+		])
+	
+	def build(self) -> List[BaseWidget]:
+		return [
+			Paragraph(self.counter),
+			Button(self.increment, children=[TextWidget("Increase")])
+		]
+	
+	def increment(self, event: DOMEvent) -> None:
+		self.counter += 1
+		print(self.counter)
+		self.update()
 
 
 render("body", App())
