bbd90f78d2f477cca7428fc6b93e6971ded08e0a
NadieFiind <nadiefiind@gmail.com>
2022-09-26 18:38:14 +0800

    Add Demo: Counter App

--- demo/counter-app/src/index.py
+++ demo/counter-app/src/index.py
@@ -1,22 +1,29 @@
 from pyfyre import render
 from pyfyre.nodes import *
+from browser import DOMEvent
 
 
 class App(Element):
 	def __init__(self) -> None:
+		self.count = 0
+		self.text = TextNode(self.count)
+		
 		super().__init__("main", children=[
-			Element("h1", children=[TextNode("Welcome to PyFyre 🐍🔥")]),
-			Element("p", children=[
-				TextNode("Learn PyFyre by reading the "),
-				Anchor(
-					"https://pyfyre.netlify.app/",
-					children=[TextNode("documentation")]
-				),
-				TextNode(". It is also advisable to learn "),
-				Anchor("https://www.brython.info/", children=[TextNode("Brython")]),
-				TextNode(" alongside PyFyre as it is built on top of Brython."),
-			])
+			Element("h1", children=[TextNode("Counter App")]),
+			Element("p", children=[self.text]),
+			Button(self.decrement, children=[TextNode("-")]),
+			Button(self.increment, children=[TextNode("+")])
 		])
+	
+	def increment(self, event: DOMEvent) -> None:
+		self.count += 1
+		self.text.set_value(self.count)
+		self.text.update_dom()
+	
+	def decrement(self, event: DOMEvent) -> None:
+		self.count -= 1
+		self.text.set_value(self.count)
+		self.text.update_dom()
 
 
 render("body", {"/": App()})
