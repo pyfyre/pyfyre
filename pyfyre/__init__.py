b9a5c326389bf8394ec03339da591e09153e25e8
NadieFiind <nadiefiind@gmail.com>
2022-10-15 14:17:56 +0800

    Add exception: render can only be called once

--- pyfyre/__init__.py
+++ pyfyre/__init__.py
@@ -2,19 +2,24 @@ from pyfyre.nodes import Node
 from typing import Dict, Callable
 from browser import document, window
 from pyfyre.router import RouteManager
-from pyfyre.exceptions import NodeNotFound
+from pyfyre.exceptions import PyFyreException, NodeNotFound
 
 __all__ = [
 	"render",
 	"PRODUCTION"
 ]
 
+_rendered = False
 PRODUCTION: bool = False
 
 
 def render(root_selector: str, routes: Dict[str, Callable[[], Node]]) -> None:
-	nodes = document.select(root_selector)
+	global _rendered
+	if _rendered:
+		raise PyFyreException("'render' function can only be called once")
+	_rendered = True
 	
+	nodes = document.select(root_selector)
 	if nodes:
 		RouteManager.set_routes(routes)
 		RouteManager.root_node = nodes[0]
