c29dbc29f9b98ba45149969c20026e15a26f2046
NadieFiind <nadiefiind@gmail.com>
2022-10-17 00:59:08 +0800

    Custom window event: pyfyreload

--- pyfyre/__init__.py
+++ pyfyre/__init__.py
@@ -1,7 +1,8 @@
 from pyfyre.nodes import Node
 from typing import Dict, Callable
-from browser import document, window
 from pyfyre.router import RouteManager
+from pyfyre.events import PyFyreEventType
+from browser import document, window, DOMEvent
 from pyfyre.exceptions import PyFyreException, NodeNotFound
 
 __all__ = [
@@ -13,15 +14,29 @@ _rendered = False
 PRODUCTION: bool = False
 
 
+def _set_custom_window_events() -> Dict[str, DOMEvent]:
+	events = {}
+	
+	for pyfyre_event in PyFyreEventType:
+		event = document.createEvent("Event")
+		event.initEvent(pyfyre_event.value, True, True)
+		events[pyfyre_event.value] = event
+	
+	return events
+
+
 def render(root_selector: str, routes: Dict[str, Callable[[], Node]]) -> None:
 	global _rendered
 	if _rendered:
 		raise PyFyreException("'render' function can only be called once")
 	_rendered = True
 	
+	custom_events = _set_custom_window_events()
+	
 	nodes = document.select(root_selector)
 	if nodes:
 		RouteManager.initialize(nodes[0], routes)
 		RouteManager.render_route(window.location.pathname)
+		window.dispatchEvent(custom_events["pyfyreload"])
 	else:
 		raise NodeNotFound(root_selector)
