ac242b1df4db0d5655eaf12f9ac7e83634d669de
NadieFiind <nadiefiind@gmail.com>
2022-10-20 13:06:28 +0800

    Refactor Element styling

--- pyfyre/nodes/base.py
+++ pyfyre/nodes/base.py
@@ -72,17 +72,16 @@ class Element(Node):
 						Element(
 							"p",
 							lambda: [TextNode("An error occurred while loading this element")],
-							attrs={
-								"style": "color: black; font-size: 1rem; "
-								"margin: 0; font-weight: bold;"
-							}
+							styles=[Style(
+								color="black", font_size="1rem", margin="0", font_weight="bold"
+							)]
 						)
 					],
-					attrs={
-						"style": "padding: 15px; background-color: white; "
-						"box-shadow: 0 0 10px #888888; border-top: 5px solid #ff726f; "
-						"border-radius: 5px; font-family: Arial; display: inline-block;"
-					}
+					styles=[Style(
+						padding="15px", background_color="white", box_shadow="0 0 10px #888888",
+						border_top="5px solid #ff726f", border_radius="5px", font_family="Arial",
+						display="inline-block"
+					)]
 				)
 			]
 		
@@ -91,35 +90,34 @@ class Element(Node):
 			Element(
 				"div",
 				lambda: [
-					Element("p", lambda: [TextNode("Unhandled Runtime Error")], attrs={
-						"style": "color: black; font-size: 1.5rem; font-weight: bold; "
-						"margin-top: 0; margin-bottom: 7px;"
-					}),
+					Element("p", lambda: [TextNode("Unhandled Runtime Error")], styles=[Style(
+						color="black", font_size="1.5rem", font_weight="bold",
+						margin_top="0", margin_bottom="7px"
+					)]),
 					Element(
 						"span", lambda: [TextNode(f"{exc_type.__name__}: {exc_value}")],
-						attrs={
-							"style": "color: #ff3131; font-weight: bold; "
-							"font-family: monospace; font-size: 0.91rem;"
-						}
+						styles=[Style(
+							color="#ff3131", font_weight="bold",
+							font_family="monospace", font_size="0.91rem"
+						)]
 					),
-					Element("p", lambda: [TextNode("Traceback")], attrs={
-						"style": "color: black; font-size: 1.3rem; font-weight: bold; "
-						"margin-bottom: 7px;"
-					}),
+					Element("p", lambda: [TextNode("Traceback")], styles=[Style(
+						color="black", font_size="1.3rem", font_weight="bold",
+						margin_bottom="7px"
+					)]),
 					Element(
 						"p",
 						lambda: [Element("pre", lambda: [TextNode(tr)])],
-						attrs={
-							"style": "background-color: black; color: white; "
-							"padding: 1px 15px"
-						}
+						styles=[Style(
+							background_color="black", color="white", padding="1px 15px"
+						)]
 					)
 				],
-				attrs={
-					"style": "padding: 15px; background-color: white; "
-					"box-shadow: 0 0 10px #888888; border-top: 5px solid #ff726f; "
-					"border-radius: 5px; font-family: Arial; display: inline-block;"
-				}
+				styles=[Style(
+					padding="15px", background_color="white", box_shadow="0 0 10px #888888",
+					border_top="5px solid #ff726f", border_radius="5px", font_family="Arial",
+					display="inline-block"
+				)]
 			)
 		]
 	
