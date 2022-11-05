3063dec8b5fb97e267d1be0fdd2b2be7d4716f8c
NadieFiind <nadiefiind@gmail.com>
2022-11-06 05:45:54 +0800

    Fix routing

--- pyfyre/router.py
+++ pyfyre/router.py
@@ -1,5 +1,5 @@
 from settings import ROUTES
-from typing import Dict, Callable, Optional
+from typing import Any, Dict, Callable, Optional
 from browser import document, window, DOMEvent
 from pyfyre.events import window_event_listener
 from pyfyre.nodes import Node, Element, TextNode
@@ -8,6 +8,7 @@ from pyfyre.nodes import Node, Element, TextNode
 class RouteManager:
     """A static class that enables navigation between various views in a PyFyre application."""
 
+    _current_route: str
     _routes_builder: Dict[str, Callable[[], Node]] = {}
     _routes: Dict[str, Optional[Node]] = {}
     _root_node = document.select_one("#root")
@@ -15,11 +16,8 @@ class RouteManager:
     @staticmethod
     def initialize(routes: Dict[str, Callable[[], Node]]) -> None:
         """:meta private:"""
+        RouteManager._current_route = RouteManager.parse_route(window.location.href)
         RouteManager._routes_builder = routes
-
-        @window_event_listener("popstate")
-        def onpopstate(event: DOMEvent) -> None:
-            RouteManager.change_route(window.location.pathname)
 
     @staticmethod
     def parse_route(route_name: str) -> str:
@@ -79,14 +77,39 @@ class RouteManager:
         RouteManager._root_node.attach(node.dom)
 
     @staticmethod
-    def change_route(route_name: str) -> None:
-        """Change the current route to ``route_name``.
-
-        This method is typically not used directly.
-        Use the ``pyfyre.nodes.RouterLink`` instead.
-        """
-        route = RouteManager.parse_route(route_name)
-        route_data = ROUTES.get(route) or {"title": "404: Page Not Found :("}
+    def _update_page(
+        route_data: Dict[str, Any], prev_route_data: Dict[str, Any]
+    ) -> bool:
+        if route_data.get("head") != prev_route_data.get("head"):
+            return False
 
         document.title = route_data.get("title")
-        RouteManager.render_route(route)
+
+        icon = document.select_one("link[rel~='icon']")
+        if icon is not None:
+            icon.href = route_data.get("icon")
+
+        return True
+
+    @staticmethod
+    def change_route(route_name: str) -> None:
+        """:meta private:"""
+        prev_route = RouteManager._current_route
+
+        if ROUTES.get(prev_route) is None:
+            window.location.reload()
+        else:
+            route_name = RouteManager.parse_route(route_name)
+            RouteManager._current_route = route_name
+            route_data = ROUTES.get(route_name)
+
+            if route_data is None:
+                window.location.reload()
+            else:
+                has_same_head = RouteManager._update_page(
+                    route_data, ROUTES[prev_route]
+                )
+                if has_same_head:
+                    RouteManager.render_route(route_name)
+                else:
+                    window.location.reload()
