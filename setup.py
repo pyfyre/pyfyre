51cfb0defdc449a5ded989869bdb843a48f24190
NadieFiind <nadiefiind@gmail.com>
2022-11-06 18:41:15 +0800

    Update setup.py Documentation URL

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     project_urls={
         "Source": "https://github.com/pyfyre/pyfyre",
         "Updates": "https://www.facebook.com/pyfyreframework/",
-        "Documentation": "https://pyfyre.netlify.app/docs/",
+        "Documentation": "https://pyfyre-docs.netlify.app/",
     },
     entry_points={"console_scripts": ["pyfyre = pyfyre_cli:execute"]},
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
