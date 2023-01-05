877338b410468a05cacb6f23ddb8364f2dce9b96
NadieFiind <nadiefiind@gmail.com>
2023-01-05 17:50:25 +0800

    Bump version to 0.6.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "PyFyre"
 copyright = "2022, StackSpace"
 author = "StackSpace"
-release = "0.6.6"
+release = "0.6.7"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
